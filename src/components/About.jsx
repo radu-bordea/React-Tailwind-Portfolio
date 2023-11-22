78b9fbbc9b2facd4545dfb97e22aafb4d45e2f9b
radu-bordea <radu.bordea.dev@gmail.com>
2023-11-22 16:57:02 +0100

    add data fro the projects section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,18 +4,25 @@ import SectionTitle from "./SectionTitle";
 
 const About = () => {
   return (
-    <section className="bg-white py-20" id="about">
+    <section className="bg-gray-950 py-20" id="about">
       <div className="align-element grid md:grid-cols-2 items-center gap-16">
         <img src={aboutSvg} className="w-full h-64" />
         <article>
           <SectionTitle text="code and code" />
           <p className="text-slate-600 leading-loose">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo
-            sed dolor, ea laboriosam odit iure maiores tempore quae officiis
-            voluptatem tempora laudantium aspernatur voluptatibus ullam id qui
-            labore? Quia modi saepe voluptatem rerum, dolores corporis sapiente
-            optio. Corrupti itaque quis deserunt fuga esse, nulla tenetur?
-            Incidunt aspernatur nisi optio fugit.
+            As a student IT developer at Åland University of Applied Sciences in
+            Finland, my passion lies in coding, and I thoroughly enjoy working
+            with technologies such as JavaScript, React, React-Native, Flutter,
+            and Node.js. I find immense satisfaction in bringing ideas to life
+            through code. Whether it's crafting interactive user interfaces with
+            React or building cross-platform mobile applications using
+            React-Native and Flutter, I am always eager to explore and apply the
+            latest tools in the ever-evolving field of web and mobile
+            development. My love for coding extends to server-side development,
+            where I leverage the efficiency and scalability of Node.js to create
+            robust backend systems. Coding, for me, is not just a skill; it's a
+            source of inspiration and a journey of self-discovery in the dynamic
+            world of software development.
           </p>
         </article>
       </div>
--- src/data.jsx
+++ src/data.jsx
@@ -1,57 +1,88 @@
-import { nanoid } from 'nanoid';
-import { FaHtml5, FaJs, FaReact } from 'react-icons/fa';
+import { nanoid } from "nanoid";
+import { FaHtml5, FaJs, FaReact } from "react-icons/fa";
+import project1 from "./assets/project1.png";
+import project2 from "./assets/project2.png";
+import project3 from "./assets/project3.png";
+import project4 from "./assets/project4.png";
+import project5 from "./assets/project5.png";
+import project6 from "./assets/project6.png";
 
 export const links = [
-  { id: nanoid(), href: '#home', text: 'home' },
-  { id: nanoid(), href: '#skills', text: 'skills' },
-  { id: nanoid(), href: '#about', text: 'about' },
-  { id: nanoid(), href: '#projects', text: 'projects' },
+  { id: nanoid(), href: "#home", text: "home" },
+  { id: nanoid(), href: "#skills", text: "skills" },
+  { id: nanoid(), href: "#about", text: "about" },
+  { id: nanoid(), href: "#projects", text: "projects" },
 ];
 
 export const skills = [
   {
     id: nanoid(),
-    title: 'HTML&CSS',
-    icon: <FaHtml5 className='h-16 w-16 text-teal-500' />,
-    text: 'As a moderate-level developer, I have gained proficiency in crafting web pages using HTML and CSS. My journey began with school projects, where I learned the fundamentals of structuring content (HTML) and styling it (CSS). Further honing my skills through Udemy courses and independent learning, I\'ve successfully created aesthetically pleasing and responsive user interfaces. My ability to blend design principles with coding ensures the delivery of visually engaging and user-friendly websites.',
+    title: "HTML&CSS",
+    icon: <FaHtml5 className="h-16 w-16 text-teal-500" />,
+    text: "As a moderate-level developer, I have gained proficiency in crafting web pages using HTML and CSS. My journey began with school projects, where I learned the fundamentals of structuring content (HTML) and styling it (CSS). Further honing my skills through Udemy courses and independent learning, I've successfully created aesthetically pleasing and responsive user interfaces. My ability to blend design principles with coding ensures the delivery of visually engaging and user-friendly websites.",
   },
   {
     id: nanoid(),
-    title: 'Javascript',
-    icon: <FaJs className='h-16 w-16 text-teal-500' />,
-    text: 'With a moderate skill level in JavaScript, I\'ve embarked on a dynamic programming journey. Starting from foundational knowledge acquired in school projects, I delved deeper into the language through Udemy courses and self-directed projects. This journey has equipped me with the ability to create interactive and feature-rich websites. Leveraging my skills, I\'ve implemented functionalities ranging from user input validation to asynchronous requests. My JavaScript proficiency is demonstrated through practical application and continuous exploration of its evolving ecosystem.',
+    title: "Javascript",
+    icon: <FaJs className="h-16 w-16 text-teal-500" />,
+    text: "With a moderate skill level in JavaScript, I've embarked on a dynamic programming journey. Starting from foundational knowledge acquired in school projects, I delved deeper into the language through Udemy courses and self-directed projects. This journey has equipped me with the ability to create interactive and feature-rich websites. Leveraging my skills, I've implemented functionalities ranging from user input validation to asynchronous requests. My JavaScript proficiency is demonstrated through practical application and continuous exploration of its evolving ecosystem.",
   },
   {
     id: nanoid(),
-    title: 'React',
-    icon: <FaReact className='h-16 w-16 text-teal-500' />,
-    text: 'Venturing into the world of frontend development, I\'ve acquired a moderate proficiency in React. My introduction to this powerful library began with school projects, and I subsequently deepened my understanding through Udemy courses and personal initiatives. Leveraging React, I\'ve developed reusable components and interactive user interfaces. My grasp of state management, component lifecycles, and routing showcases my ability to create dynamic, efficient, and scalable web applications. As a developer, I\'m committed to staying abreast of React\'s latest advancements and refining my skills through hands-on experience..',
+    title: "React",
+    icon: <FaReact className="h-16 w-16 text-teal-500" />,
+    text: "Venturing into the world of frontend development, I've acquired a moderate proficiency in React. My introduction to this powerful library began with school projects, and I subsequently deepened my understanding through Udemy courses and personal initiatives. Leveraging React, I've developed reusable components and interactive user interfaces. My grasp of state management, component lifecycles, and routing showcases my ability to create dynamic, efficient, and scalable web applications. As a developer, I'm committed to staying abreast of React's latest advancements and refining my skills through hands-on experience..",
   },
 ];
 
 export const projects = [
   {
     id: nanoid(),
-    img: 'https://images.pexels.com/photos/326503/pexels-photo-326503.jpeg?auto=compress&cs=tinysrgb&w=800',
-    url: 'https://react-projects.netlify.app/',
-    github: 'https://github.com/john-smilga',
-    title: 'first project',
-    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores aperiam porro impedit tenetur quo hic omnis doloribus dolores enim deleniti.',
+    img: { project1 },
+    url: "https://react-menu-categories.netlify.app/",
+    github: "https://github.com/radu-bordea/React-Menu.git",
+    title: "first project",
+    text: "Crafted with elegance, this React-based menu app presents a delightful collection of recipe cards. Seamlessly categorized, it allows users to explore and select their preferred recipes, ensuring a tasteful culinary experience.",
   },
   {
     id: nanoid(),
-    img: 'https://images.pexels.com/photos/2148222/pexels-photo-2148222.jpeg?auto=compress&cs=tinysrgb&w=800',
-    url: 'https://react-projects.netlify.app/',
-    github: 'https://github.com/john-smilga',
-    title: 'second project',
-    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores aperiam porro impedit tenetur quo hic omnis doloribus dolores enim deleniti.',
+    img: { project2 },
+    url: "https://react-typescript-todo-list-task.netlify.app/",
+    github: "https://github.com/radu-bordea/ReactTypescript-todoList.git",
+    title: "second project",
+    text: "Elevating task management, this React-driven todo list combines functionality with style. The intuitive drag-and-drop feature and local storage integration provide a streamlined experience, making organization a seamless and visually pleasing task.",
   },
   {
     id: nanoid(),
-    img: 'https://images.pexels.com/photos/12883026/pexels-photo-12883026.jpeg?auto=compress&cs=tinysrgb&w=800',
-    url: 'https://react-projects.netlify.app/',
-    github: 'https://github.com/john-smilga',
-    title: 'third project',
-    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores aperiam porro impedit tenetur quo hic omnis doloribus dolores enim deleniti.',
+    img: { project3 },
+    url: "https://react-tours-offer.netlify.app/",
+    github: "https://github.com/radu-bordea/React-Tours.git",
+    title: "third project",
+    text: "Navigate through travel possibilities effortlessly with this React-powered tour list. Featuring interactive cards, users can easily delete unwanted options, ensuring a personalized exploration experience. It's a sleek solution for planning your next adventure.",
+  },
+  {
+    id: nanoid(),
+    img: { project4 },
+    url: "https://react-reviews-users.netlify.app/",
+    github: "https://github.com/radu-bordea/React-Reviews.git",
+    title: "fourth project",
+    text: "Experience the elegance of user interaction with this React-based profile card component. Seamlessly cycle through profiles with buttons for previous, next, or even a random selection. The design ensures a visually appealing and engaging journey through user profiles.",
+  },
+  {
+    id: nanoid(),
+    img: { project5 },
+    url: "https://portofolio-radu-bordea.netlify.app/",
+    github: "https://github.com/radu-bordea/Portofolio-Radu.B.git",
+    title: "fifth project",
+    text: "Immerse yourself in the elegance of my portfolio website, where the fusion of HTML, CSS, JavaScript, and React creates a captivating user experience. From seamless navigation to visually stunning displays, this website showcases the art of web development.",
+  },
+  {
+    id: nanoid(),
+    img: { project6 },
+    url: "https://online-education-javascript.netlify.app/",
+    github:
+      "https://github.com/radu-bordea/OnlineEducationResponsiveDesign.git",
+    title: "sixth project",
+    text: "Designed for learning with finesse, this web design platform elegantly combines HTML, CSS, and jQuery. Providing an enriching experience, it empowers users to grasp the intricacies of web design, making education a visually pleasing and interactive journey.",
   },
 ];
