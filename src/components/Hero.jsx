c47e133ea9bed96395b458b952d8ff7890baab6c
radu-bordea <radu.bordea.dev@gmail.com>
2023-11-23 00:53:23 +0100

    update github and linkedin links

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,9 @@ import heroImg from "../assets/hero.svg";
 import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
 
 const Hero = () => {
+  const github = "https://github.com/radu-bordea"; // Replace with your GitHub profile URL
+  const linkedin = "https://www.linkedin.com/in/radu-nicolae-bordea-695155220/"; // Replace with your LinkedIn profile URL
+
   return (
     <div className="bg-indigo-950 py-24">
       <div className="align-element grid md:grid-cols-2 items-center gap-8">
@@ -15,16 +18,16 @@ const Hero = () => {
             turning ideas into interactive reality
           </p>
           <div className="flex gap-x-4 mt-4">
-            <a href="#">
-              <FaGithubSquare className="h-8 w-8 text-slate-500 hover:text-black duration-300" />
+            <a href={github}>
+              <FaGithubSquare className="h-8 w-8 text-slate-500 hover:text-black duration-300" />{" "}
             </a>
-            <a href="#">
+            <a href={linkedin}>
               <FaLinkedin className="h-8 w-8 text-slate-500 hover:text-black duration-300" />
             </a>
           </div>
         </article>
         <article className="hidden md:block">
-          <img src={heroImg} className="h-80 lg:h-96" />
+          <img src={heroImg} className="h-80 lg:h-96" alt="Hero" />
         </article>
       </div>
     </div>
